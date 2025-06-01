b4fb86bdb3966b01986e00b4a67a751c65bc9fed
Parth Rakholiya <parthrakholiya2000@gmail.com>
2025-06-01 12:18:41 +0530

    Added query files
    MongoDB query files added

---
+++ 01_database_collection.js
@@ -0,0 +1,9 @@
+//? 1:  Creating / Deleting Databases
+// show dbs
+// use Products
+// db.dropDatabase()
+
+//? 2:  Creating / Deleting Databases and Collections
+// db.createCollection('test')
+// show collections
+// db.test.drop() // where the test is the collection name
